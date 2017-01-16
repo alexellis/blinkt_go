080074e5e9a647c67a992561281cb75858f0b1d5
Alex Ellis <AlexEllis2@gmail.com>
2017-01-16 17:21:04 +0000

    SetPixel / Clear method

--- app.go
+++ app.go
@@ -1,86 +1,104 @@
 package main
 
 import (
- . "github.com/alexellis/rpi"
- "fmt"
+	. "github.com/alexellis/rpi"
 )
 
 const DAT int = 23
 const CLK int = 24
 
 func show(pixels [8][4]int) {
-    eof(32)
-    for i,_ := range pixels {
-       brightness := pixels[i][3]
-       r := pixels[i][0]
-       g := pixels[i][1]
-       b := pixels[i][2]
+	sof()
+	for i, _ := range pixels {
+		brightness := pixels[i][3]
+		r := pixels[i][0]
+		g := pixels[i][1]
+		b := pixels[i][2]
 
-       // 0b11100000 (224)
-       bitwise := 224
-       writeByte(bitwise | brightness)
-       writeByte(b)
-       writeByte(g)
-       writeByte(r)
-    }
-    eof(36)
+		// 0b11100000 (224)
+		bitwise := 224
+		writeByte(bitwise | brightness)
+		writeByte(b)
+		writeByte(g)
+		writeByte(r)
+	}
+	eof()
 }
 
 func initPixels(brightness int) [8][4]int {
-    var pixels [8][4] int
-    for i,_ := range pixels {
-       pixels[i][0] = 0
-       pixels[i][1] = 0
-       pixels[i][2] = 0
-       pixels[i][3] = brightness
-    }
-    return pixels
+	var pixels [8][4]int
+	for i, _ := range pixels {
+		pixels[i][0] = 0
+		pixels[i][1] = 0
+		pixels[i][2] = 0
+		pixels[i][3] = brightness
+	}
+	return pixels
 }
 
-func eof(pulses int) {
-     DigitalWrite(GpioToPin(DAT), 0)
-     for i := 0 ; i < pulses ; i++ {
-       DigitalWrite(GpioToPin(CLK), 1)
-       DigitalWrite(GpioToPin(CLK), 0)
-     }
+func pulse(pulses int) {
+	DigitalWrite(GpioToPin(DAT), 0)
+	for i := 0; i < pulses; i++ {
+		DigitalWrite(GpioToPin(CLK), 1)
+		DigitalWrite(GpioToPin(CLK), 0)
+	}
+}
+
+func eof() {
+	pulse(36)
+}
+
+func sof() {
+	pulse(32)
 }
 
 func setup() {
-  PinMode(GpioToPin(DAT), OUTPUT)
-  PinMode(GpioToPin(CLK), OUTPUT)
+	PinMode(GpioToPin(DAT), OUTPUT)
+	PinMode(GpioToPin(CLK), OUTPUT)
 }
 
 func writeByte(val int) {
-  for i := 0; i < 8; i++ {
-	// 0b10000000 = 128
-     DigitalWrite(GpioToPin(DAT), val & 128)
-     DigitalWrite(GpioToPin(CLK), 1)
-     val = val << 1
-     DigitalWrite(GpioToPin(CLK), 0)
-  }
+	for i := 0; i < 8; i++ {
+		// 0b10000000 = 128
+		DigitalWrite(GpioToPin(DAT), val&128)
+		DigitalWrite(GpioToPin(CLK), 1)
+		val = val << 1
+		DigitalWrite(GpioToPin(CLK), 0)
+	}
+}
+
+func Clear(pixels *[8][4]int) {
+	r := 0
+	g := 0
+	b := 0
+	for i := 0; i < 8; i++ {
+		SetPixel(pixels, i, r, g, b)
+	}
+}
+
+func SetPixel(pixels *[8][4]int, p int, r int, g int, b int) {
+	pixels[p][0] = r
+	pixels[p][1] = g
+	pixels[p][2] = b
 }
 
 func main() {
-    WiringPiSetup()
+	WiringPiSetup()
+	pixels := initPixels(50)
+	setup()
 
-    pixels := initPixels(50)    
-    setup()
-    fmt.Println(pixels)
-    Delay(100)
+	Delay(100)
 
-    pixels[0][1] = 128 & 255 // 0xff = 255
-    pixels[1][1] = 128 & 255 // 0xff = 255
-    pixels[5][1] = 128 & 255 // 0xff = 255
-    pixels[6][1] = 128 & 255 // 0xff = 255
+	r := 255
+	g := 0
+	b := 0
+	for pixel := 0; pixel < 8; pixel++ {
+		SetPixel(&pixels, pixel, r, g, b)
+		show(pixels)
+		Delay(100)
+	}
 
-    show(pixels)    
-    Delay(1000)
-    pixels[0][1] = 0 & 255 // 0xff = 255
-    pixels[1][1] = 0 & 255 // 0xff = 255
-    pixels[5][1] = 0 & 255 // 0xff = 255
-    pixels[6][1] = 0 & 255 // 0xff = 255
-
-    show(pixels)    
-
+	Delay(1000)
+	Clear(&pixels)
+	show(pixels)
 }
-
